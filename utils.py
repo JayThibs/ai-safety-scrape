c5cea2b52af8b68c83b056786520ab9a79fb53af
JayThibs <thibo.jacques@gmail.com>
2022-02-27 23:15:27 -0400

    Re-added deleted functions and classes.

--- utils.py
+++ utils.py
@@ -35,6 +35,14 @@ def fread(fname):
         return fh.read()
 
 
+class each:
+    def __init__(self, f):
+        self.f = f
+
+    def __rrshift__(self, other):
+        return list(map(self.f, other))
+
+
 class filt:
     def __init__(self, f):
         self.f = f
@@ -73,3 +81,105 @@ class join:
             return self.sep.join(other)
         except:
             return None
+
+
+Y = object()
+
+
+def id(x):
+    return x
+
+
+class Reflective:
+    def __getattribute__(self, f):
+        def _fn(*args, **kwargs):
+            return lambda x: x.__getattribute__(f)(*args, **kwargs)
+
+        return _fn
+
+    def __getitem__(self, a):
+        return lambda x: x[a]
+
+    def __mul__(self, other):
+        if other == Y:
+
+            def _f(x, y=None):
+                if y == None:
+                    x, y = x
+
+                return x * y
+
+            return _f
+
+        return lambda x: x * other
+
+    def __rmul__(self, other):
+        if other == Y:
+
+            def _f(x, y=None):
+                if y == None:
+                    x, y = x
+
+                return y * x
+
+            return _f
+
+        return lambda x: other * x
+
+    def __add__(self, other):
+        if other == Y:
+
+            def _f(x, y=None):
+                if y == None:
+                    x, y = x
+
+                return x + y
+
+            return _f
+
+        return lambda x: x + other
+
+    def __radd__(self, other):
+        if other == Y:
+
+            def _f(x, y=None):
+                if y == None:
+                    x, y = x
+
+                return y + x
+
+            return _f
+
+        return lambda x: other + x
+
+
+# (b -> a -> b) -> b -> [a] -> b
+def foldl(f, init, arr):
+    curr = init
+    for elem in arr:
+        curr = f(curr, elem)
+    return curr
+
+
+# (a -> b -> b) -> b -> [a] -> b
+def foldr(f, init, arr):
+    curr = init
+    for elem in arr[::-1]:
+        curr = f(elem, curr)
+    return curr
+
+
+def comp(*fs):
+    if len(fs) == 1:
+        return fs[0]
+
+    def _f(x):
+        for f in fs[::-1]:
+            x = f(x)
+
+        return x
+
+    return _f
+
+
+X = Reflective()
