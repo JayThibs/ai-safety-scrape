99c2ac401663a6598a95c4882eb76793e1302f28
JayThibs <thibo.jacques@gmail.com>
2022-03-14 16:45:16 -0300

    Removed functions to clean up main script.

--- arxiv_extractor.py
+++ arxiv_extractor.py
@@ -13,196 +13,7 @@ import pandas as pd
 import traceback
 
 
-sh("mkdir -p tmp tmp2 out done fallback_needed errored")
-
-
-def any_to_utf8(b):
-    """Detects encoding and converts to utf-8."""
-    try:
-        return b.decode("utf-8")
-    except UnicodeDecodeError:
-        # try to figure out encoding if not utf-8
-        guess = chardet.detect(b)["encoding"]
-        if not guess or guess == "UTF-8":
-            return
-        try:
-            return b.decode(guess)
-        except (UnicodeDecodeError, LookupError):
-            # still cant figure out encoding, give up
-            return
-
-
-def convert_to_utf8(rootdir="."):
-    """Converts all files in root folder to utf-8."""
-    for doc in ls(rootdir):
-        if doc.endswith(".tex"):
-            try:
-                with open(doc, "rb") as fh:
-                    b = fh.read()
-                    cont = any_to_utf8(b)
-                    if cont is None:
-                        return
-                fwrite(doc, cont)
-            except ExitCodeError:
-                traceback.print_exc()
-                print(f"Error converting {doc}, will go to /fallback_needed.")
-                print("Error converting files to utf-8.")
-
-
-def preextract_tar(dump):
-    """
-    Creates tmp2/{dump_name} directory and extracts tar files and copies them to tmp2/dump_name/*.
-    Creates tmp2/done_{dump_name} file to signal copy_tar that extraction is done.
-    """
-    dump_name = dump.split("/")[-1][:-4]
-    sh(
-        f"(mkdir -p tmp2/{dump_name}; tar xf {dump} -C tmp2/{dump_name} && touch tmp2/done_{dump_name}; echo finished preload of {dump_name}) &"
-    )
-
-
-def copy_tar(dump):
-    """Copies tar files from tmp2/{dump_name}/* to tmp/."""
-    dump_name = dump.split("/")[-1][:-4]
-    # print(dump_name)
-    for i in range(120):
-        if os.path.exists(f"tmp2/done_{dump_name}"):
-            sh(f"mv tmp2/{dump_name}/* tmp")
-            return True
-        print("waiting for tar...")
-        time.sleep(1)
-
-    return False
-
-
-def mv_files_to_root(rootdir="tmp"):
-    """Moves all files in root folder subdirectories to root folder."""
-    for doc in ls(rootdir):
-        try:
-            if os.path.isdir(doc):
-                sh(f"find ./{doc} -type f -print0 | xargs -0 mv -t .")
-                sh(f"rm -rf {doc}")
-        except ExitCodeError:
-            traceback.print_exc()
-            print(
-                "Error moving files to root folder. Likely because there's a file with the same name in the root folder."
-            )
-
-
-def convert_semiauto(rootdir="tmp", paper_id=paper_id, main_tex_dict=main_tex_dict):
-    """
-    Converts paper tex files semi-automatically. If there are multiple tex files,
-    it will check for a list of common "main" file names and use the first one found.
-    If there are multiple .tex files and it cannot find a main file, you will be prompted
-    to select one.
-    """
-    print('Changing current directory to "tmp"...')
-    os.chdir(rootdir)
-    main_match = False
-    print("Current directory: " + os.getcwd())
-    print("paper_id: " + paper_id)
-
-    try:
-        assert len(ls(".")) > 0
-        convert_to_utf8(rootdir=".")
-        if len(ls(".")) == 1:
-            # if there is only one tex file, just convert it
-            main_match = True
-            doc = ls(".")[0].split("/")[-1]
-            sh(f"timeout 7s pandoc -s {doc} -o {paper_id}.md --wrap=none")
-        else:
-            # if there are multiple tex files,
-            # check for the main file based on a common list of names
-            for doc in ls("."):
-                doc = doc.split("/")[-1][:-4]
-                # print(doc)
-                if doc in [
-                    "main",
-                    "Main",
-                    "MAIN",
-                    "_main",
-                    "paper",
-                    "Paper",
-                    "PAPER",
-                    "ms",
-                    "arxiv",
-                    "arXiv",
-                    "example_paper",
-                    "root",
-                    "example",
-                    "00_main",
-                    "00_Main",
-                    "00-Main",
-                    "00-main",
-                    "main_arxiv",
-                    "main_arXiv",
-                    "main-arxiv",
-                    "main-arXiv",
-                    "Main-arXiv",
-                    "master",
-                    "Master",
-                ]:
-                    # if there is a common main file name, use it
-                    main_match = True
-                    sh(f"timeout 7s pandoc -s {doc}.tex -o {paper_id}.md --wrap=none")
-                    break
-        if not main_match:
-            tmp_contents = os.listdir()
-            num_tex_files = 0
-            print(os.listdir())
-            for doc in tmp_contents:
-                if doc.endswith(".tex"):
-                    num_tex_files += 1
-                    tex_doc = doc
-            if num_tex_files == 1:
-                # if there is only one tex file, use it
-                sh(f"timeout 7s pandoc -s {tex_doc} -o {paper_id}.md --wrap=none")
-            else:
-                if paper_id in main_tex_dict:
-                    # if main file was stored in main_tex_dict, use it
-                    main_tex = main_tex_dict[paper_id]
-                else:
-                    # if there are multiple tex files
-                    # and it's not in the above list: prompt user to select one
-                    print("Multiple tex files found. Please select the main file: ")
-                    main_tex = str(
-                        input(
-                            f"Enter the main .tex filename here, file extension included (e.g. AIProgress.tex): "
-                        )
-                    )
-                    main_tex_dict[paper_id] = main_tex
-                    os.chdir("..")
-                    json.dump(main_tex_dict, open("main_tex_dict.json", "w"))
-                    os.chdir(rootdir)
-
-                sh(f"timeout 7s pandoc -s {main_tex} -o {paper_id}.md --wrap=none")
-
-        os.chdir("..")
-        print("Current directory: " + os.getcwd())
-        sh(f"mv tmp/{paper_id}.md out/{paper_id}.md")
-
-    except:
-        traceback.print_exc()
-        print("Error converting paper. Moving to fallback pile...")
-        if os.getcwd().split("/")[-1] == "tmp":
-            os.chdir("..")
-        # fallback:
-        try:
-            # move to fallback pile so we can handle it later
-            sh(
-                f"mkdir -p fallback_needed/{paper_id} && mv tmp/* fallback_needed/{paper_id}/"
-            )
-        except ExitCodeError:
-            traceback.print_exc()
-
-        assert os.path.exists(f"out/{paper_id}.md")  # to send tar to errored pile
-
-
-def get_arxiv_ids(bib_file_path):
-    with open(bib_file_path, "r") as f:
-        bib_string = f.read()
-    return re.findall(r"(?:arXiv:|abs/)(\d{4}\.\d{4,5})", bib_string), bib_string
-
-
+sh("mkdir -p tmp out done fallback_needed errored && rm -rf tmp/*")
 files = ls("files")
 ignore_filenames = pd.read_csv("ignore_filenames.csv").values
 arxiv_citations_list = []
@@ -212,14 +23,27 @@ if os.path.exists("main_tex_dict.json"):
 else:
     main_tex_dict = {}
 
-sh("rm -rf tmp/* tmp2/*")
-preextract_tar(files[0])
+pool = mp.Pool(processes=mp.cpu_count())
+
+def worker():
+    """worker function"""
+    print 'Worker'
+    return
+
+if __name__ == '__main__':
+    jobs = []
+    for i in range(len(files)):
+        p = mp.Process(target=preextract_tar, args=(files[i],))
+        jobs.append(p)
+        p.start()
+        p.join()
+        print(f"{i}/{len(files)}")
+
 
 for i, dump in enumerate(tqdm(files)):
-    # extracts tar files to tmp2/{dump_name}/*
+    # extracts tar files to tmp/{dump_name}/*
+
     paper_id = dump.split("/")[-1][:-4]
-    if i + 1 < len(files):
-        preextract_tar(files[i + 1])
     try:
         # clear tmp every loop to process only one set of .tex files at a time
         sh("rm -rf tmp/*")
@@ -264,8 +88,8 @@ for i, dump in enumerate(tqdm(files)):
                         sh(f"rm {doc[:-3]}")
 
                 elif doc.endswith(".tex"):
-                    # if tex, keep it
-                    sh(f"mv {doc} {doc}")
+                    # if tex, do nothing and keep it
+                    pass
 
                 elif doc.endswith(".bbl"):
                     # if bbl, extract arxiv ids from citations, add to list, and delete bbl
@@ -287,6 +111,8 @@ for i, dump in enumerate(tqdm(files)):
         mv_files_to_root()
         print("Converting paper...")
         convert_semiauto("tmp", paper_id, main_tex_dict)
+        list_of_paper_folders = ls("tmp")
+        pool.map(convert, list_of_paper_folders)
 
         sh(f"mv {dump} done")
         print(f"marking {dump} as done")
