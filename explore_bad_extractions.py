435de79de973fa90bd7d24c731ea7738726ce29d
JayThibs <thibo.jacques@gmail.com>
2022-04-04 20:02:23 -0300

    Script for exploring bad extractions.

--- explore_bad_extractions.py
+++ explore_bad_extractions.py
@@ -1,8 +1,17 @@
 import json
 
 arxiv = json.load(open("arxiv_data_list.json"))
-
+i = 0
+j = 0
 for paper in arxiv:
-    if paper["citation_level"] == "1" and paper["good_extraction"] == False:
-        print(paper["id"])
-        print(paper["text"])
+    if paper["citation_level"] == "1":
+        # print(paper["id"])
+        # print(paper["text"])
+        if len(paper["text"]) < 500 and paper["main_tex_filename"] != "":
+            i += 1
+            print(paper["id"])
+            print(paper["text"])
+        j += 1
+
+print(i)
+print(j)
